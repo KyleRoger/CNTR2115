8643db9ccae344e47fbe5df7b4b535ffe44f99da
akraa <arie.kraayenbrink@gmail.com>
2019-01-08 15:36:00 -0500

    Add files

---
+++ client/src/main.c
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+#ifdef _WIN32
+
+#endif
+
+#ifdef DEBUG
+	printf("%s\n", "Hello, World!");
+#endif
+
+#ifdef linux
+
+#endif	
---
+++ makefile.sh
@@ -0,0 +1,61 @@
+# !/bin/bash
+
+# --------------------------------------------------------------------------------------------------------------------------------
+# Author:		Arie Kraayenbrink
+# Date:			April 19, 2018
+# File Name:	makefile.sh
+# Description:	This script will run the make files within the following folders: chat-server, chat-client and common
+#				It will also copy the executables to the common/bin folder and make sure they have permsions to run.
+# --------------------------------------------------------------------------------------------------------------------------------
+
+
+
+if test "$1" = "clean"
+then
+
+	# run the makefile in common folder
+	cd common
+	make clean
+	cd ..
+
+	# run the makefile in chat-server folder
+	cd chat-server
+	make clean
+	cd ..
+
+	# run the makefile in chat-client folder
+	cd chat-client
+	make clean
+	cd ..
+
+	exit 1
+fi
+
+
+
+# run the makefile in common folder
+cd common
+make
+cd ..
+
+# run the makefile in chat-server folder
+cd chat-server
+make
+cd ..
+
+# run the makefile in chat-client folder
+cd chat-client
+make
+cd ..
+
+
+
+# copy all exicutables into the common/bin folder
+cp chat-server/bin/chat-server common/bin
+cp chat-client/bin/chat-client common/bin
+
+
+
+# make files executable
+chmod +x common/bin/chat-server
+chmod +x common/bin/chat-client
