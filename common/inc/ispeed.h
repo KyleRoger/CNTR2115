deaf457703c398dc60f17b128aefb38d58bd92d1
Arie Kraayenbrink <20170212+akraa@users.noreply.github.com>
2019-01-09 13:14:38 -0500

    create ispeed.h
    Move include headers into new ispeed.h

---
+++ common/inc/ispeed.h
@@ -0,0 +1,24 @@
+
+
+
+
+/********PRE-PROCESSORS*******/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <errno.h>
