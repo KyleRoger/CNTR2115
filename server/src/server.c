5b54b46faf990925c9200d55f2fb9ad8999b8c11
Arie Kraayenbrink <20170212+akraa@users.noreply.github.com>
2019-01-08 21:47:45 -0500

    A start on the server
    Mostly windows stuff

---
+++ server/src/server.c
@@ -0,0 +1,65 @@
+/*
+* -----------------------------------------------------------------------
+* File			: makefile
+* Project		: ispeed
+* Author 		: Arie Kraayenbrink
+* Editors		: Arie kraayenbrink
+* Date 			: Jan. 8, 2019
+* Description 	: This makefile will compile and link all source files
+*             	: in the server part of the system.  
+*				: 
+*				: Steps for creating windows sockets on server.
+*				   1. Initialize Winsock.
+*				   2. Create a socket.
+*				   3. Bind the socket.
+*				   4. Listen on the socket for a client.
+*				   5. Accept a connection from a client.
+*				   6. Receive and send data.
+*				   7. Disconnect.
+*
+* Credit		: https://docs.microsoft.com/en-us/windows/desktop/winsock/getting-started-with-winsock
+* ------------------------------------------------------------------------
+*/
+
+#include "../inc/server.h"
+
+
+int server()
+{
+	int success = FAILURE;
+
+
+	#ifdef _WIN32
+	WSADATA wsaData;
+
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0) 
+	{
+	    printf("WSAStartup failed: %d\n", iResult);
+	    return 1;
+	}
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof (hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, PORT, &hints, &result);
+	if (iResult != 0) {
+	    printf("getaddrinfo failed: %d\n", iResult);
+	    WSACleanup();
+	    return 1;
+	}
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	#endif
+
+	return success;
+}
