64165420af23683d65d9095f4b7334bb58aa1c9f
Arie Kraayenbrink <20170212+akraa@users.noreply.github.com>
2019-01-21 21:59:49 -0500

    windows batch file

--- ispeed.bat
+++ ispeed.bat
@@ -4,7 +4,8 @@ REM FILE			ispeed.bat
 REM PROJECT			ispeed
 REM PROGRAMMER		Arie Kraayenbrink
 REM FIRST VERSION	Jan. 21, 2019
-REM DESCRIPTION		Start the ispeed server and client
+REM DESCRIPTION		Start the ispeed server or client based on the input parameters provided.
+REM					The client and server are responsible to validate the input parameters.
 REM -------------------------------------------------------------------------
 
 
@@ -16,5 +17,7 @@ if %1 == -p (
 	cd client\bin
 	ispeed.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 )
+
+cd ..\..
 
 :END
